98a5dec0af785f515d523fb276afe11c62b4921d
removedporn <86824510+removedporn@users.noreply.github.com>
2023-03-01 02:18:41 +0000

    Delete chaincode directory

--- chaincode/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const usercontract = require('./userContract.js');
-const registrarcontract = require('./registrarContract.js');
-
-module.exports.contracts = [usercontract, registrarcontract];
-
-
